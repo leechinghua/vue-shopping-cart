230253d79659b91abcb4153e95ea96822f6d75ba
leechinghua <leechenghua107@gmail.com>
2025-03-04 12:03:04 +0800

    建立基本結構

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,17 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="min-h-screen bg-gray-100">
+    <NavBar :cartItemCount="cartItems.length" />
+    <router-view 
+      :courses="courses" 
+      :cartItems="cartItems" 
+      @add-to-cart="addToCart" 
+      @remove-from-cart="removeFromCart"
+      @remove-selected="removeSelected"
+      @checkout="checkout"
+    />
+  </div>
 </template>
 
 <style scoped>
